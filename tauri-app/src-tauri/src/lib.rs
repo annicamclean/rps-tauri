dc4d555f8317168c1db622ea470367d65e10e889
annicamclean <annicamcl@tutanota.com>
2025-05-27 01:57:41 -0400

    Added most of the functionality

--- tauri-app/src-tauri/src/lib.rs
+++ tauri-app/src-tauri/src/lib.rs
@@ -4,11 +4,39 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+//0 = tie, 1 = win, -1 = loss
+//0 = rock, 1 = paper, 2 = scissors
+#[tauri::command]
+fn play_game(weapon: i32, computer_weapon: i32) -> i32 {
+    if weapon == computer_weapon {
+        return 0;
+    } else if weapon == 0 {
+        if computer_weapon == 1 {
+            return -1;
+        } else if computer_weapon == 2 {
+            return 1;
+        }   
+    } else if weapon == 1 {
+        if computer_weapon == 0 {
+            return 1;
+        } else if computer_weapon == 2 {
+            return -1;
+        }
+    } else if weapon == 2 {
+        if computer_weapon == 0 {
+            return -1;
+        } else if computer_weapon == 1 {
+            return 1;
+        }
+    }
+    return 2;
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, play_game])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- tauri-app/src/main.ts
+++ tauri-app/src/main.ts
@@ -2,6 +2,7 @@ import { invoke } from "@tauri-apps/api/core";
 
 let greetInputEl: HTMLInputElement | null;
 let greetMsgEl: HTMLElement | null;
+let chosen: boolean = false;
 
 async function greet() {
   if (greetMsgEl && greetInputEl) {
@@ -12,11 +13,125 @@ async function greet() {
   }
 }
 
+let shuffleNum: number = 0;
+let intervalId: number;
+
+async function shufflePic(): Promise<void> {
+  return new Promise((resolve) => {
+    let questionMark = document.getElementById("question-mark") as HTMLImageElement;
+    if (shuffleNum !== 6) {
+      if (shuffleNum % 3 === 0) {
+        if (questionMark) questionMark.src = "/src/assets/rock.PNG";
+      } else if (shuffleNum % 3 === 1) {
+        if (questionMark) questionMark.src = "/src/assets/paper.PNG";
+      } else {
+        if (questionMark) questionMark.src = "/src/assets/scissors.PNG";
+      }
+      shuffleNum++;
+    } else {
+      clearInterval(intervalId);
+      resolve();
+    }
+  });
+}
+
+async function computerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  let randomChoice = choices[Math.floor(Math.random() * 3)];
+  let questionMark = document.getElementById("question-mark") as HTMLImageElement;
+  intervalId = setInterval(shufflePic, 400);
+  await new Promise(resolve => setTimeout(resolve, 2400)); // Wait for 6 shuffles (6 * 400ms)
+  // Convert string choice to number
+  questionMark.classList.add('chosen');
+  switch (randomChoice) {
+    case "rock":
+      if (questionMark) questionMark.src = "/src/assets/rock.png";
+      console.log(questionMark.src);
+      return 0;
+    case "paper": 
+      if (questionMark) questionMark.src = "/src/assets/paper.png";
+      console.log(questionMark.src);
+      return 1;
+    case "scissors": 
+      if (questionMark) questionMark.src = "/src/assets/scissors.png";
+      console.log(questionMark.src);
+      return 2;
+    default: 
+      return 3;
+  }
+}
+
+async function playGame(weapon: number) {
+  let computerWeapon = await computerChoice();
+  console.log("Computer Choice: ", computerWeapon);
+  let result = await invoke("play_game", {
+    weapon: weapon,
+    computerWeapon: computerWeapon,
+  });
+  if (result === 0) {
+    // Tie
+  } else if (result === 1) {
+    // Win
+  } else {
+    // Loss
+  }
+}
+
 window.addEventListener("DOMContentLoaded", () => {
   greetInputEl = document.querySelector("#greet-input");
   greetMsgEl = document.querySelector("#greet-msg");
   document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
     e.preventDefault();
     greet();
+  });
+
+  const rock = document.querySelector("#rock");
+  const paper = document.querySelector("#paper");
+  const scissors = document.querySelector("#scissors");
+
+  rock?.addEventListener("click", () => {
+    if (!chosen) {
+      rock.classList.add("chosen");
+      paper?.classList.add("not-chosen");
+      scissors?.classList.add("not-chosen");
+      chosen = true;
+      playGame(0);
+    }
+  });
+
+  paper?.addEventListener("click", () => {
+    if (!chosen) {
+      paper.classList.add("chosen");
+      rock?.classList.add("not-chosen");
+      scissors?.classList.add("not-chosen");
+      chosen = true;
+      playGame(1);
+    }
+  });
+
+  scissors?.addEventListener("click", () => {
+    if (!chosen) {
+      scissors.classList.add("chosen");
+      rock?.classList.add("not-chosen");
+      paper?.classList.add("not-chosen");
+      chosen = true;
+      playGame(2);
+    }
+  });
+  
+
+  let playAgain = document.getElementById("play-again");
+
+  playAgain?.addEventListener("click", () => {
+    let previoslyChosen = document.querySelectorAll(".chosen");
+    let nonPreviouslyChosen = document.querySelectorAll(".not-chosen");
+    previoslyChosen.forEach(element => {
+      element.classList.remove("chosen");
+    });
+    nonPreviouslyChosen.forEach(element => {
+      element.classList.remove("not-chosen");
+    });
+    chosen = false;
+    shuffleNum = 0;
   });
 });
